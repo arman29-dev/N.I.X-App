c4828b46ecfc7562beb2fe9a0c503091dda1b182
Arman Das <armandevilk282004@gmail.com>
2025-09-28 23:10:06 +0530

    ➖ print statement

--- lib/utils/launchers.dart
+++ lib/utils/launchers.dart
@@ -1,6 +1,3 @@
-// A utility file to handle URL launching.
-
-import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 // The URL to be opened.
@@ -10,10 +7,6 @@ final Uri registrationUri = Uri.parse('https://quiet-pup-summary.ngrok-free.app/
 // It checks if the URL can be launched before attempting to do so.
 Future<void> launchUrlInBrowser() async {
   if (!await launchUrl(registrationUri)) {
-    // If the URL cannot be launched, throw an exception.
-    if (kDebugMode) {
-      print('Could not launch $registrationUri');
-    }
     throw Exception('Could not launch $registrationUri');
   }
 }
