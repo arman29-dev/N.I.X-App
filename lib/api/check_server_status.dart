0a1a751be1fa61ef802b4041e96b997be33b99bf
Arman Das <armandevilk282004@gmail.com>
2025-09-28 23:11:30 +0530

    ⚙️✏️ ping endpoint url name

--- lib/api/check_server_status.dart
+++ lib/api/check_server_status.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 Future<bool> getServerStatus () async {
   final response = await http.get(
-    Uri.parse('https://quiet-pup-summary.ngrok-free.app/server-status'),
+    Uri.parse('https://quiet-pup-summary.ngrok-free.app/ping'),
   );
 
   return response.statusCode == 200;
